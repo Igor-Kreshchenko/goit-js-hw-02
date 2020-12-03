29bc4fe83926b87fd484fa08f5425a664f497cb8
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2020-12-03 17:41:35 +0200

    Выполнил задание 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const formatString = function (string) {
+  const array = string.split('');
 
+  for (let i = 0; i < array.length; i += 1) {
+    if (i > 40) {
+      array.splice(40, i, '...');
+    }
+  }
+
+  const finalString = array.join('');
+  return finalString;
+};
+
+console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+
+console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+
+console.log(formatString('Curabitur ligula sapien.'));
+
+console.log(
+  formatString(
+    'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+  ),
+);
