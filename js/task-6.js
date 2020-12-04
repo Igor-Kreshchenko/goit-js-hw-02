bf93059451f3434c7f3ec151d936676db5bb57d7
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2020-12-04 12:44:31 +0200

    Выполнил задание 6

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,25 @@
+let input;
+const numbers = [];
+let total = 0;
 
+while (true) {
+  input = prompt('Введите число');
+  const value = Number(input);
+
+  const notANumber = Number.isNaN(value);
+  if (notANumber) {
+    alert('Было введено не число, попробуйте еще раз');
+    continue;
+  }
+
+  numbers.push(value);
+
+  if (input === null) {
+    for (const number of numbers) {
+      total += number;
+    }
+    break;
+  }
+}
+
+console.log(`Общая сумма чисел равна ${total}`);
