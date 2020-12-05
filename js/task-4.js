58ec85c94b649c83c084772e6772618a82cd2802
Igor Kreshchenko <Igor.kreshchenko@gmail.com>
2020-12-05 11:46:48 +0200

    Добавил решение задания 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,29 @@
 const formatString = function (string) {
-  if (string.length > 40) {
-    const array = string.split('');
-    let changedArray;
-
-    for (let i = 0; i < array.length; i++) {
-      if (i > 40) {
-        changedArray = array.splice(40, i, '...');
-        const shortString = array.join('');
-        return shortString;
-      }
-    }
+  const maxStringLength = 40;
+  if (string.length > maxStringLength) {
+    const shortString = string.slice(0, 40) + '...';
+    return shortString;
   }
 
   return string;
 };
+
+// const formatString = function (string) {
+//   if (string.length > 40) {
+//     const array = string.split('');
+//     let changedArray;
+
+//     for (let i = 0; i < array.length; i++) {
+//       if (i > 40) {
+//         changedArray = array.splice(40, i, '...');
+//         const shortString = array.join('');
+//         return shortString;
+//       }
+//     }
+//   }
+
+//   return string;
+// };
 
 console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
 
